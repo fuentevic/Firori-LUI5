8485b8bf24e79bce462a5823cc564b2895b76973
Victor Fuente <victor.fuente-franco@capgemini.com>
2026-09-16 11:22:22 +0000

    Get/Set text with button onPress function

--- module1/webapp/controller/Main.controller.js
+++ module1/webapp/controller/Main.controller.js
@@ -4,7 +4,17 @@ sap.ui.define([
     "use strict";
 
     return Controller.extend("namespace.module1.controller.Main", {
-        onInit() {
+        onInit() 
+        {
+            
+        },
+
+        onPress: function (){
+            var txtIzquierda = this.getView().byId("idSaludo").getText();
+            var txtDerecha = this.getView().byId("idSubtitle").getText();
+
+            this.getView().byId("idSubtitle").setText(txtIzquierda);
+            this.getView().byId("idSaludo").setText(txtDerecha);
         }
     });
 });
