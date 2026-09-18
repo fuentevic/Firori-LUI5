07b95c1011ae3a336d049ad06853f399366e595c
Victor Fuente <victor.fuente-franco@capgemini.com>
2026-09-18 12:04:02 +0000

    Add Panels for homeworks

--- module1/webapp/controller/Main.controller.js
+++ module1/webapp/controller/Main.controller.js
@@ -15,6 +15,16 @@ sap.ui.define([
 
             this.getView().byId("idSubtitle").setText(txtIzquierda);
             this.getView().byId("idSaludo").setText(txtDerecha);
+        },
+
+        onDesplazar(){
+            let sIzqTexto = this.getView().byId("primerTextoEJ1").getText();
+            this.getView().byId("segundoTextoEJ1").setText(sIzqTexto);
+        },
+
+        onAnhadir: function (){
+            let oInput = this.getView().byId("inputEJ1");
+            this.getView().byId("primerTextoEJ1").setText(oInput.getValue());
         }
     });
 });
